45881189e61977c640043f41c4b1de388d0ff76f
miller11 <ross-h-miller@uiowa.edu>
2018-09-26 21:47:18 -0500

    Everything up to Section 2.8 and updating navbar code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
+    <div class="container">
+      <pp-header></pp-header>
+    </div>
 
+    <div class="container">
+      <pp-menu></pp-menu>
+    </div>
+
+    <div class="container">
+      <pp-footer></pp-footer>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Header from "./components/Header"
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Menu from './components/Menu'
 
+export default {
+  components: {
+    ppHeader: Header,
+    ppFooter: Footer,
+    ppHome: Home,
+    ppMenu: Menu
+  }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
 <template>
-    
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">PIZZA PLANET</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </header>
 </template>
 
 <script>
@@ -8,6 +27,13 @@
     }
 </script>
 
-<style scoped>
+<style>
+  header {
+    margin-bottom: 20px;
+  }
+
+  .navbar-brand {
+    font-size: 1.5em;
+  }
 
 </style>
