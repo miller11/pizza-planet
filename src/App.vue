6e67ca5edcf8246e70f34efac3c5ee6ca728dd5f
miller11 <ross-h-miller@uiowa.edu>
2018-10-05 21:46:33 -0500

    everything through section 4 and video 43

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,22 @@
     </div>
 
     <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <router-view name="ordering-guide"></router-view>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+          <router-view name="delivery"></router-view>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+          <router-view name="history"></router-view>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
       <pp-footer></pp-footer>
     </div>
   </div>
@@ -24,6 +40,7 @@ export default {
     ppFooter: Footer
   }
 }
+
 </script>
 
 <style>
